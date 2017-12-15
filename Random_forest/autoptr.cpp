9cbed2fc0fc363d3ea878878913008e384e78a05
Yong Feng <286606198@qq.com>
2017-12-15 13:29:52 +0800

    Update autoptr.cpp

--- Random_forest/autoptr.cpp
+++ Random_forest/autoptr.cpp
@@ -1,30 +1,22 @@
 #include<iostream>
-#include<memory>
-#include<windows.h>
-#include<vector>
-#include<string>
-#include<limits>
-#include<map>
-#include <fstream>
-#include <algorithm>
-#include <iterator>
-#include <memory>
-#include <Windows.h> //ssize_t
-#include<time.h>
-#include<iomanip>
-#include <cmath>
 using namespace std;
 
 
-int autoptr() {
+int autoptr()
+{
 	std::auto_ptr<int> p(new int);
-	std::auto_ptr<int> p2=std::auto_ptr<int>(0);
-	*p.get() = 100;  //p.get() ->pointer to int,if false return 0/nullptr
-  //reference:http://www.cplusplus.com/reference/memory/auto_ptr/get/
+	std::auto_ptr<int> p2=std::auto_ptr<int>(nullptr);
+    std::cout << "p2 points is null " << std::boolalpha <<(p2.get() == nullptr) << '\n';
 
-	std::cout << "p points to " << *p.get() << '\n';
-	std::cout << "p points " << p.get() << '\n';
-	std::cout << "p2 points " << (p2.get()==nullptr)<< '\n';
-  }
+    *p.get() = 100;
+
+    std::cout << "p points to " << *p.get() << '\n';
+    std::cout << "p points " << p.get() << '\n';
+
+    int* p3;
+    p3 = p.release();
+    std::cout << "p3 points to " << *p3 << '\n';
+    std::cout << "p points is null " << std::boolalpha <<(p.get() == NULL) << '\n';
+}
   
   
