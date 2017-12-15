3b443c72f196472451fa57230cd138dc10a7e1bb
Yong Feng <286606198@qq.com>
2017-12-15 09:45:03 +0800

    Update time.cpp

--- Random_forest/time.cpp
+++ Random_forest/time.cpp
@@ -1,15 +1,15 @@
-	#include<iostream>
-  #include<time.h>
-  using namespace std;
+#include<iostream>
+#include<time.h>
+using namespace std;
   
-  int  time()
-  {
-    //http://blog.csdn.net/luoweifu/article/details/20288549
-	  cout<< "dec:" <<dec<<time(NULL)<<endl;
-	  time_t timep;
-	  time(&timep);
-	  cout << gmtime(&timep)->tm_year;
-	  srand((unsigned int)time(&timep));
-	  cout << rand()<<" "<<rand()<<endl;
+int  time()
+{
+	//http://blog.csdn.net/luoweifu/article/details/20288549
+	cout<< "dec:" <<dec<<time(NULL)<<endl;
+	time_t timep;
+	time(&timep);
+	cout << gmtime(&timep)->tm_year;
+	srand((unsigned int)time(&timep));
+	cout << rand()<<" "<<rand()<<endl;
     return 0;
-  }
+ }
