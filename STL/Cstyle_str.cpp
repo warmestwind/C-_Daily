02ec1459d1c87f3bab1f321c30fe7b1b00dcf15d
Yong Feng <286606198@qq.com>
2018-06-20 16:11:04 +0800

    Create Cstyle_str.cpp

---
+++ STL/Cstyle_str.cpp
@@ -0,0 +1,17 @@
+#include<iostream>  
+#include<string>
+
+using namespace std;
+
+int main()
+{   
+    
+    string a = "hahah";
+    char b[5] = "asdf";
+    printf("%s", a.c_str());
+    printf("%s", b);
+    string c = b;
+    cout << c;
+    getchar();  
+    
+}
